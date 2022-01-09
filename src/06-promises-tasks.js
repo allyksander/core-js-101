b1d6a2c5b9dfd155343cb9c8d18509728ab1e77c
PanchenkoAleksandr <aleksandr.panchenko@tut.by>
2022-01-09 22:14:38 +0300

    feat: implement 01-03 task from 06-promisese-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,22 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    switch (isPositiveAnswer) {
+      case true:
+        resolve('Hooray!!! She said "Yes"!');
+        break;
+
+      case false:
+        resolve('Oh no, she said "No".');
+        break;
+
+      default:
+        reject(Error('Wrong parameter is passed! Ask her again.'));
+        break;
+    }
+  });
 }
 
 
@@ -48,8 +62,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +85,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
